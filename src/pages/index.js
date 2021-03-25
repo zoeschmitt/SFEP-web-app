a2d3443b939443c5f8d7b3e7570087abfdaa23d1
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-03-25 00:30:57 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    // const toggle = () => {
+    //     setIsOpen(!isOpen)
+    // }
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Home
