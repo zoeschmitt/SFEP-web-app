3d7f90690af9869219925c794aa69f40b1518db3
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-03-30 12:57:11 -0500

    added navbar sidebar and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { animateScroll as scroll } from 'react-scroll';
+import { Link as LinkS } from 'react-scroll'
+
+const Footer = () => {
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    };
+    return (
+        <div className="footer-wrapper">
+            <footer className="footer-container">
+                <div className="content-container">
+                    <LinkS className="logo-container" to='/' onClick={toggleHome}>
+                        <img className="logo-image" src="" alt="logo" />
+                    </LinkS>
+                </div>
+                <div className="content-container">
+                    <p className="footer-sub-heading">Company Name</p>
+                    <LinkS className="footer-link" to="about" smooth={true} duration={500} spy={true} exact='true'>Home</LinkS>
+                </div>
+                <div className="content-container">
+                    <p className="footer-sub-heading">Get in Touch</p>
+                    <p className="footer-link" to="about" smooth={true} duration={500} spy={true} exact='true'>(512) - 566 - 8921</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+import SideBar from '../components/SideBar'
 
 const Home = () => {
-    // const toggle = () => {
-    //     setIsOpen(!isOpen)
-    // }
+    const [isOpen, setIsOpen] = useState(false) // sidebar
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <>
-        
+        <SideBar isOpen={isOpen} toggle={toggle} />
+            <NavBar toggle={toggle} />
+            
+            <Footer />
         </>
     )
 }
