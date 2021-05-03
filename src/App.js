886f7368aee0bc4ed1fe1732ddb9dbaf8eb399d3
Adam Emil Castillo <aec153@txstate.edu>
2021-05-03 17:44:44 -0500

    leaving this here just in case

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
-import Home from './pages';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
     <Router>
+      <Switch>
+      <Route path="/login">
+      <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
       <Home />
+      </Switch>
     </Router>
   );
 }
 
 export default App;
 
-//Hi from Jules
