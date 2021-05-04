9250318331042c5cadfb5f055ca241f225af5432
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-05-03 20:39:42 -0500

    added api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import DiscussionContainer from '../../components/DiscussionContainer'
 import Footer from '../../components/Footer'
 import NavBar from '../../components/NavBar'
 import PostContainer from '../../components/PostContainer'
 import SideBar from '../../components/SideBar'
-import { posts } from '../testData'
+import PostService from '../../services/postService'
 import './styles.css';
 
 const Home = () => {
+    const postService = new PostService();
     const [isOpen, setIsOpen] = useState(false) // sidebar
+    const [posts, setPosts] = useState([]) //posts[0]
     const [selectedPost, setSelectedPost] = useState(null) //posts[0]
     const toggle = () => {
         setIsOpen(!isOpen)
     }
+
+
+    const getPosts = useCallback(async () => {
+        try {
+            var posts = await postService.getAllPosts();
+            console.log(posts.posts.posts);
+            setPosts(posts.posts.posts);
+        } catch (e) {
+            console.log(e);
+        }
+    }, [])
+
+
+    //this loads when the page loads
+    useEffect(() => {
+        getPosts();
+    }, [])
 
     const updateSelectedPost = (post) => {
         setSelectedPost(post);
@@ -26,12 +45,12 @@ const Home = () => {
             <div className="posts-discussion-wrapper">
                 <ul className="posts-list-container">
                     {posts.map(post => (
-                        <li key={post.id} onClick={() => updateSelectedPost(post)}>
+                        <li key={post._id} onClick={() => updateSelectedPost(post)}>
                             <PostContainer post={post} />
                         </li>
                     ))}
                 </ul>
-                {/* <DiscussionContainer /> */}
+                <DiscussionContainer />
             </div>
             <Footer />
         </>
